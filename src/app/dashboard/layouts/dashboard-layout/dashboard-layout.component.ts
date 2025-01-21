9e9e27b615ea11e270cd7b9ae1bdb2b1b5e4419d
Vijardy Garcia <VijardyGarcia@dEErvieWvg@2023>
2025-01-21 10:08:11 -0600

    feat: redireccion y protecion de pases

--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -11,4 +11,8 @@ export class DashboardLayoutComponent {
   private authService = inject(AuthService)
 
   public user = computed(() => this.authService.currentUser());
+
+  onLogout(){
+    this.authService.logout();
+  }
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  baseUrl: 'http://localhost:3000'
+  baseUrl: 'https://backendnest-production-8749.up.railway.app'
 }
